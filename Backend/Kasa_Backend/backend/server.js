429e1cd95d26bfb0f9ebaf9fd3e1e5ee1646850d
רועי משולם <roimac@MacBook-Pro-sl-rwy.local>
2025-09-03 00:20:46 +0300

    test mode deploy1

--- Backend/Kasa_Backend/backend/server.js
+++ Backend/Kasa_Backend/backend/server.js
@@ -10,7 +10,7 @@ const server = http.createServer(app);
 // Initialize WebSocket
 initWebSocket(server);
 
-server.listen(PORT, () => {
+server.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is running on port ${PORT}`);
 });
 
